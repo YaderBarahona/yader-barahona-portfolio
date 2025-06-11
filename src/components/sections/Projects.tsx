788bcc122b027ec302e6d344fb8b4feb514430cd
Yader <yaderbarahona180@gmail.com>
2025-06-11 10:40:58 -0600

    projects update

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -18,7 +18,7 @@ const Projects = () => {
 
   return (
     <section className="bg-yellow px-3 py-5 dark:bg-[#494949] md:px-0 md:py-10">
-      <h2 className="py-10 text-center text-xl font-medium uppercase tracking-widest text-black">
+      <h2 className="py-10 text-center text-xl font-medium uppercase tracking-widest text-black dark:text-white">
         {t("projects.title")}
       </h2>
       <div className="flex flex-wrap justify-center gap-10">
@@ -33,9 +33,9 @@ const Projects = () => {
               src={item.images[0]}
               alt="Profile"
             />
-            <span className="mt-3 rounded-b-lg bg-[#696969] px-4 pb-1 pt-2 text-center text-lg text-white">
+            {/* <span className="mt-3 rounded-b-lg bg-[#696969] px-4 pb-1 pt-2 text-center text-lg text-white">
               {item.startDate}
-            </span>
+            </span> */}
             <p className="font-xl mt-2 font-bold uppercase tracking-wide dark:text-white">
               {item.title}
             </p>
